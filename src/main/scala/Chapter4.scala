9ed9a88c413802089267b0ec9313172547b097db
Mike Bridge <mike@bridgecanada.com>
2016-12-07 13:07:45 -0700

    chapter 4

---
+++ src/main/scala/Chapter4.scala
@@ -0,0 +1,37 @@
+import javax.crypto.spec.SecretKeySpec
+import javax.crypto.{Cipher, SecretKey}
+
+import com.bridgecanada.utils.HexConversions._
+
+object Chapter3 {
+
+  // Question 3.8
+  def decrypt38(hexCipherText: String, hexKey: String): String = {
+    val cipher = Cipher.getInstance("AES/ECB/NoPadding")
+    val secretKey = new SecretKeySpec(hexKey.asBytes, "AES")
+    cipher.init(Cipher.DECRYPT_MODE, secretKey)
+    cipher.doFinal(hexCipherText.asBytes).asHexString
+  }
+
+  // Question 3.9
+  def encrypt39(hexPlainText: String, hexKey: String): String = {
+    val cipher = Cipher.getInstance("AES/ECB/NoPadding")
+    val secretKey = new SecretKeySpec(hexKey.asBytes, "AES")
+    cipher.init(Cipher.ENCRYPT_MODE, secretKey)
+    cipher.doFinal(hexPlainText.asBytes).asHexString
+  }
+
+  // Question 3.10
+  def encrypt310(hexPlainText: String, secretKey: SecretKey): String = {
+    val cipher = Cipher.getInstance("DES/ECB/NoPadding")
+    cipher.init(Cipher.ENCRYPT_MODE, secretKey)
+    cipher.doFinal(hexPlainText.asBytes).asHexString
+  }
+
+
+
+
+  def secretKeySpec(key: String): SecretKeySpec =
+    new SecretKeySpec(key.asBytes, "AES")
+}
+
